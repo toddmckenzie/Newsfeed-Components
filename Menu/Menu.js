4af0382f88059a5727020e47d613b27915991dfc
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-04-10 15:31:45 -0500

    added articles component

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,7 +2,7 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
 
-  menu.classList.toggle('menu--open')
+  menu.classList.toggle('menu--open');
 }
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
